0afe3d59487df44b4201146b899eb9208f3b43e6
HP <martinsonyedikachi@gmail.com>
2020-06-24 17:12:54 +0100

    Edited Contact control for nodemailer

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const contact = require('./Contact/index')
 const jetblogs = require('./Components/Blogs/index')
-const jetcontact = require('./Components/Contact/ContactControl')
 
 const cors = require('cors');
 const helmet = require('helmet');
 const server = express();
 
+server.engine('handlebars', exphbs());
+server.set('view engine', 'handlebars');
+
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use('/api', jetblogs);
-server.use('/contact', jetcontact);
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+server.use('/contact', contact)
 
 
 server.get('/', (req, res) => {
